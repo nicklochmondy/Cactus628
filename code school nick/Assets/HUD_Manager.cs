8c985afbaf3fcd11db65ffa01dfd1914ed5542ad
nicklochmondy <40363964+nicklochmondy@users.noreply.github.com>
2018-06-28 11:29:34 -0400

    628

---
+++ code school nick/Assets/HUD_Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HUD_Manager : MonoBehaviour
+{
+    public int StartingHealth;
+    int CurrentHealth;
+    int score;
+    public Text scoretext;
+    public Slider healthbar;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        CurrentHealth = StartingHealth;
+        healthbar.value = CurrentHealth;
+    }
+
+    public void UpdateScore(int amount)
+
+    {
+        score = score + amount;
+        scoretext.text = "Score:" + score;
+    }
+
+    public void UpdateHealth(int amount)
+    {
+        CurrentHealth = CurrentHealth + amount;
+        healthbar.value = CurrentHealth;
+        if (CurrentHealth <= 0)
+            Death();
+    }
+
+    void Death()
+    {
+        SceneManager.LoadScene("level01");
+    }
+}
